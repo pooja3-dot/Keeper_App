8817740c1194fa8bb17700ec281825f23d9e63ea
pooja <78692569+pooja3-dot@users.noreply.github.com>
2022-03-16 22:53:03 +0530

    Add initial files in it

---
+++ build/precache-manifest.90a7d5e807f33d5453b8e08da23e6ee8.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "062e4d0d2a4b25a5b32c02a0e7499c2a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9ec186b49af4acb93d04",
+    "url": "/static/js/2.06b12e55.chunk.js"
+  },
+  {
+    "revision": "1ebbf38b953b8aad174b",
+    "url": "/static/js/main.3bf36285.chunk.js"
+  },
+  {
+    "revision": "ff7a3412b3d387d84984",
+    "url": "/static/js/runtime-main.45b30dc6.js"
+  }
+]);
